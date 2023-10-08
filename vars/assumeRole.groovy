27f3b382e2afd6d703d0e6b0bd710a4646e30511
Nari26 <43762809+Nari26@users.noreply.github.com>
2023-10-08 15:36:47 -0700

    Update assumeRole.groovy

--- vars/assumeRole.groovy
+++ vars/assumeRole.groovy
@@ -1,7 +1,7 @@
 def call(roleCredentials){
     try {
-        println("Role ARN: " + roleArn)
-        def credsJson = sh (script : "#!/bin/bash -e\naws sts assume-role --role-arn ${roleArn} --role-session-name DevOpsRoleSession", returnStdout: true).trim()
+        println("Role ARN: " + roleCredentials)
+        def credsJson = sh (script : "#!/bin/bash -e\naws sts assume-role --role-arn ${roleCredentials} --role-session-name DevOpsRoleSession", returnStdout: true).trim()
         def jsonSlurper = new groovy.json.JsonSlurperClassic()
         def credsObj = jsonSlurper.parseText(credsJson)
         env.accessKeyId = credsObj.Credentials.AccessKeyId
